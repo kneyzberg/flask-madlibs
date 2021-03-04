6d31107e5caac2ef6bc239560cd1967509d96a14
Kathrin Neyzberg <kneyzberg@gmail.com>
2021-03-03 16:06:34 -0800

    first part of madlibs page

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def index():
+    """Home page showing Madlibs form"""
+    story = silly_story
+    prompts = story.prompts
+    return render_template("questions.html",form_prompts = prompts)
